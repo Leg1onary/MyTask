26f81cf009356d981e824acba625851d56d6c372
Sergey <shserg94@gmail.com>
2018-04-19 01:14:46 +0300

    without Delete

--- app/app.js
+++ app/app.js
@@ -4,8 +4,21 @@
 var myApp = angular.module('myApp',[]);
 
 
-myApp.controller('PersonCtrl', function ($scope, $http) {
+myApp.controller('myappCtrl', function ($scope, $http) {
     $http.get('person.json').success(function(data) {
         $scope.person = data;
+        $scope.metaData = [];
+        for (var i = 0; i < $scope.person.data.metaData.length; i++) {
+            $scope.metaData.push($scope.person.data.metaData[i].name);
+        };
+        $scope.clients = [];
+        $scope.personInfo = {};
+        for (var i = 0; i < $scope.person.data.rows.length; i++ ) {
+           for (var j = 0; j < $scope.metaData.length; j++) {
+            $scope.personInfo[$scope.metaData[j]] = $scope.person.data.rows[i][j];
+           }
+           $scope.clients.push($scope.personInfo);
+           $scope.personInfo = {};
+        }
     });
 });
