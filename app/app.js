5e28c810e1d03f57f3a7e6000e04e0ef48f57961
Sergey <shserg94@gmail.com>
2018-04-20 13:01:37 +0300

    added comments and minor changes

--- app/app.js
+++ app/app.js
@@ -8,11 +8,13 @@ myApp.controller('myappCtrl', function ($scope, $http) {
     $http.get('person.json').success(function(data) {
         $scope.person = data;
         $scope.metaData = [];
+        //формирование заголовка (извлечение метаданных из json)
         for (var i = 0; i < $scope.person.data.metaData.length; i++) {
             $scope.metaData.push($scope.person.data.metaData[i].name);
         };
         $scope.clients = [];
         $scope.personInfo = {};
+        //формирование ассоциативного массива клиентов в формате [{key-value,..},{key-value,..},..]
         for (var i = 0; i < $scope.person.data.rows.length; i++ ) {
            for (var j = 0; j < $scope.metaData.length; j++) {
             $scope.personInfo[$scope.metaData[j]] = $scope.person.data.rows[i][j];
@@ -20,12 +22,14 @@ myApp.controller('myappCtrl', function ($scope, $http) {
            $scope.clients.push($scope.personInfo);
            $scope.personInfo = {};
         }
+        //удаление клиента из таблицы
         $scope.archive = function (indx) {
             $scope.clients.splice(indx,1);
         };
+        //добавление клиента в таблицу
         $scope.addClient = function() {
-            $scope.clients.push($scope.newClient);
-            $scope.newClient = '';
+
         }
+
     });
 });
